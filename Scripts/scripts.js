729f57c8dafd13dd11e0608b7285b32c4fe96cc7
Gabriel Teixeira da Rocha <65963370+gabrielol113@users.noreply.github.com>
2021-08-28 21:14:43 -0300

    Adicionado um sistema de carrinho
    Adicionado um sistema de carrinho usando JavaScript com alguns bugs mas irei resolver isso!

--- Scripts/scripts.js
+++ Scripts/scripts.js
@@ -1,28 +1,82 @@
 /*Declaração de estoque */
 
-let casadinho = 50,pudim = 50,brigadeiroT = 50,redVelvet = 50,palhaItaliana = 50, tLimao = 50,Dark = 50,Morango = 50,Opereta = 50,Pimenta = 50,Caramelo = 50,Quindim = 50,Maracuja = 50;
+var bombom = [0,
+            /*casadinho = */50,
+            /*pudim = */50,
+            /*brigadeiroT = */50,
+            /*redVelvet = */50,
+            /*palhaItaliana = */50,
+            /* tLimao = */50,
+            /*Dark = */50,
+            /*Morango = */50,
+            /*Opereta = */50,
+            /*Pimenta = */50,
+            /*Caramelo = */50,
+            /*Quindim = */50,
+            /*Maracuja = */50];
+var nameBombom = [" ","Casadinho","Pudim",
+                  "Brigadeiro Tradicional",
+                  "Red Velvet", "Palha Italiana",
+                  "Torta de limão","Dark",
+                  "Morango","Opereta","Pimenta",
+                  "Caramelho","Quindim","Maracujá"];
+var carro = [0,0,0,0,0,0,0,0,0,0,0,0,0];
+var i;
 
-function adicionaBombon(let x){
-    pedido = 50;
-    pedido = pedido + 1;
-    console.log("Função funcionando");
-    return pedido;
 
+function adicionaBombon(pedido){
+
+    if(bombom[pedido]<=0){
+        alert("Estoque insuficiente!");
+    }
+    else{
+        bombom[pedido]--;
+        carro[pedido] = carro[pedido] + 1;
+        console.log("Carrinho" + carro[pedido]);
+        console.log("Estoque" + bombom[pedido]);
+        exibeCarrinho();
+       // alert("Pedido adicionado ao carrinho!");
+    }
 }
 
-function tiraBombon(){
-        pedido = 50;
-        pedido = pedido - 1;
-        console.log(pedido);
-        return pedido;
+function tiraBombon(pedido){
+     if(carro[pedido] < 1){
+         alert("Não existe o bombom " + nameBombom[pedido] + " no carrinho" );
+         
+     }
+        bombom[pedido]++;
+        carro[pedido] = carro[pedido] - 1;
+      //  alert("Produto retirado do carrinho!");
+        exibeCarrinho();
+}
 
+function exibeCarrinho(){
+
+var j = bombom.length;
+let paragrafo = document.querySelector('#carro');
+paragrafo.innerHTML = " ";
+    for( i = 1; i < j - 1; i++){
+
+           if(carro[i] != 0 && carro[i] > 0){
+                paragrafo.innerHTML = paragrafo.innerHTML + nameBombom[i]+ ": " + carro[i] + '<br>';
+            }
+            else{
+
+            }
+ 
+        
+    }
+}
+
+function enviaForm(){
+    
+    console.log("Cliente: " + name + "\n");
+    console.log("Pedido: \n");
+    console.log("Cliente:" + name + "\n");
+    e.preventDefault();
 }
 
 function mostrarValor() {
     alert(document.getElementById("nome").value);
 }
 
-document.getElementById("send").onclick = function(e) {
-    mostrarValor();
-    e.preventDefault();
-}
